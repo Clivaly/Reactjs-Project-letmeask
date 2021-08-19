8a102b254aff229d4853697b9a0aad43c4a90f32
Clivaly-Cardoso <clivalydev@gmail.com>
2021-08-19 18:21:39 -0300

    criando a primeira sala

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,36 @@
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+
+import '../styles/room.scss';
+
 export function Room() {
   return(
-    <div>Sala</div>
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="letmeask" />
+          <RoomCode />
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala test</h1>
+          <span>3 perguntas</span>
+        </div>
+        
+        <form>
+          <textarea
+         placeholder="O que voçê quer perguntar?"
+          />
+
+          <div className="form-footer">
+            <span>Para enviar sua pergunta, <button>faça seu login</button>.</span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>
+    </div>
   );
 }
